179dc176f72ca7f5ded62829e3023320082ba884
Go Watanabe <1570475+wtnbgo@users.noreply.github.com>
2026-05-03 00:05:55 +0900

    ビルド構成調整

--- main.cpp
+++ main.cpp
@@ -2,34 +2,6 @@
 #include "LayerExDraw.hpp"
 #include <vector>
 
-/**
- * ログ出力用
- */
-void
-message_log(const char* format, ...)
-{
-	va_list args;
-	va_start(args, format);
-	char msg[1024];
-	_vsnprintf_s(msg, 1024, _TRUNCATE, format, args);
-	TVPAddLog(ttstr(msg));
-	va_end(args);
-}
-
-/**
- * エラーログ出力用
- */
-void
-error_log(const char* format, ...)
-{
-	va_list args;
-	va_start(args, format);
-	char msg[1024];
-	_vsnprintf_s(msg, 1024, _TRUNCATE, format, args);
-	TVPAddImportantLog(ttstr(msg));
-	va_end(args);
-}
-
 extern void initThorvg();
 extern void deInitThorvg();
 
